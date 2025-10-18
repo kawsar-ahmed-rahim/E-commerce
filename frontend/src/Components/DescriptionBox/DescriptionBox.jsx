01d53c990f3c28cb9e166d697703e8e1fc00cb4a
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2025-10-18 10:27:32 +0600

    still now

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DescriptionBox.css";
+export const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
+          cumque corporis optio illum reiciendis. Placeat quis, unde aliquid
+          consequuntur asperiores nihil ut incidunt! Odit id cupiditate non
+          modi, quasi repellendus repellat omnis ratione provident odio
+          dignissimos officia doloremque sapiente eius iste neque accusamus eum
+          quo deleniti ipsa? Ad, accusamus veniam quasi magnam totam, ut ipsum
+          distinctio, aspernatur omnis libero voluptates! Enim in, obcaecati
+          mollitia nisi quaerat reiciendis? Obcaecati pariatur nisi provident
+          tempora perspiciatis aut sunt eum, accusamus, quod alias quos vel.
+          Dolore architecto sint aliquam ex vel hic labore beatae excepturi quas
+          inventore praesentium, dicta deserunt dolorum odit illo! Assumenda?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+          praesentium debitis iste expedita vel sapiente, sequi neque fugit quia
+          aperiam deleniti architecto ipsum perferendis officiis, perspiciatis
+          iure. Perspiciatis, non voluptas ipsum minus tempora labore, a veniam
+          sed, atque modi pariatur.
+        </p>
+      </div>
+    </div>
+  );
+};
