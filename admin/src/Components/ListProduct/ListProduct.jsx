9cb70b6ae25b9a3098dc6e28d20e2c4c9d1fbc74
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2025-10-26 20:15:53 +0600

    till now

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Listproduct.css'
+const ListProduct = () => {
+  return (
+    <div className='list-product'>ListProduct</div>
+  )
+}
+
+export default ListProduct
